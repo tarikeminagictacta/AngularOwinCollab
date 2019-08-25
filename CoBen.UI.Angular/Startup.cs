9394267f6d4a76a17c15db249d61a40be4d204cc
Tarik Eminagić <eminagic.tarik@gmail.com>
2019-08-25 14:24:56 +0200

    tada

---
+++ CoBen.UI.Angular/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System;
+using System.IO;
+
+[assembly: OwinStartup(typeof(CoBen.UI.Angular.Startup))]
+
+namespace CoBen.UI.Angular
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            var physicalFileSystem = new PhysicalFileSystem(Path.Combine(root, "wwwroot"));
+            var options = new FileServerOptions
+            {
+                RequestPath = PathString.Empty,
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            app.UseFileServer(options);
+        }
+    }
+}
